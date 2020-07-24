b024af7da0e334a7c08e5559bc5e59d10e0cd5bc
Prakashperiyasamy <praku998@gmail.com>
2020-07-24 06:57:40 +0530

    update

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -8,24 +8,6 @@ const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
 
-// @route    GET api/auth
-// @desc     Get user by token
-// @access   Private
-// router.get('/', auth, async (req, res) => {
-//   try {
-//     const user = await User.findById(req.user.id).select('-password');
-//     res.status(200).json(user);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send('Server Error');
-//   }
-// });
-
-// // @route    POST api/auth
-// // @desc     Authenticate user & get token
-// // @access   Public
-
-
 router.post(
   '/',
   [
--- routes/api/callDetails.js
+++ routes/api/callDetails.js
@@ -137,8 +137,7 @@ router.put('/updatecontact/:contactId', [
 
 router.delete('/deleteContact/:contactId', async (req, res) => {
   try {
-    // const { customerId } = req.params;
-
+  
     let customer = await CallDetails.findByIdAndRemove(req.params.contactId);
     console.log(customer)
     if (!customer) {
@@ -175,79 +174,10 @@ router.get('/allcontact', async (req, res) => {
         errors: []
       });
     }
-
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server error');
   }
-
 })
-
-router.get('/me', auth, async (req, res) => {
-  try {
-    const profile = await Profile.findOne({
-      user: req.user.id
-    }).populate('user', ['firstName', 'lastName', 'email']);
-
-    if (!profile) {
-      return res.status(400).json({ msg: 'There is no profile for this user' });
-    }
-
-    res.status(200).json(profile);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-});
-
-// @route    POST api/profile
-// @desc     Create or update user profile
-// @access   Private
-router.post('/', auth, async (req, res) => {
-  const errors = validationResult(req);
-  if (!errors.isEmpty()) {
-    return res.status(400).json({ errors: errors.array() });
-  }
-
-  const profileFields = {
-    user: req.user.id,
-  };
-
-  try {
-    // Using upsert option (creates new doc if no match is found):
-    let profile = await Profile.findOneAndUpdate(
-      { user: req.user.id },
-      { $set: profileFields },
-      { new: true, upsert: true }
-    );
-
-    res.status(201).json(profile);
-  } catch (err) {
-    console.error(err.message);
-    res.status(500).send('Server Error');
-  }
-}
-);
-
-// @route    GET api/profile/user/:user_id
-// @desc     Get profile by user ID
-// @access   Public
-router.get('/user/:user_id', async (req, res) => {
-  try {
-    const profile = await Profile.findOne({
-      user: req.params.user_id
-    }).populate('user', ['name']);
-
-    if (!profile) return res.status(400).json({ msg: 'Profile not found' });
-
-    res.json(profile);
-  } catch (err) {
-    console.error(err.message);
-    if (err.kind == 'ObjectId') {
-      return res.status(400).json({ msg: 'Profile not found' });
-    }
-    res.status(500).send('Server Error');
-  }
-});
 
 module.exports = router;
